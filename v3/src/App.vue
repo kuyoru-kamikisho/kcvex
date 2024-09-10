e26fda61a37e4a8791779b7465f8281116545536
kuyoru <kuyoru@kamikisho.com>
2024-09-10 17:38:50 +0800

    v3完成

--- v3/src/App.vue
+++ v3/src/App.vue
@@ -1,11 +1,60 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import VExpandTransition from "@/VExpandTransition.vue";
 
+const xss = ref([true, true])
 </script>
 
 <template>
-
+  <div class="container">
+    <button @click="xss[0]=!xss[0]">X测试</button>
+    <button @click="xss[1]=!xss[1]">Y测试</button>
+    <div class="test-box">
+      <div class="test-unit unit1">
+        <v-expand-transition direction="x">
+          <div v-if="xss[0]" class="box1 u-box">
+            X收缩
+          </div>
+        </v-expand-transition>
+      </div>
+      <div class="test-unit unit2">
+        <v-expand-transition>
+          <div v-if="xss[1]" class="box2 u-box">
+            Y收缩
+          </div>
+        </v-expand-transition>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
+.container {
+  width: 50vw;
+  height: 50vh;
+  margin: 15vh auto;
+  background-color: rgba(0, 0, 0, 0.15);
+}
 
+.test-box {
+  height: 93%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.test-unit {
+  flex-grow: 1;
+  max-width: 50%;
+  height: 100%;
+  border: 1px solid rgba(44, 43, 52, 0.39);
+}
+
+.u-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.46);
+}
 </style>
--- v3/src/VExpandTransition.vue
+++ v3/src/VExpandTransition.vue
@@ -1,11 +1,91 @@
 <script setup lang="ts">
+import {camelize, type PropType} from 'vue'
+import type {HTMLExpandElement} from "@/types";
 
+const props = defineProps({
+  direction: {type: String as PropType<'x' | 'y'>, default: 'y'},
+  mode: {type: String as PropType<'out-in' | 'in-out'>, default: "out-in"}
+});
+
+const transitionClass = `expand-${props.direction}-transition`
+const sizeProperty = props.direction === 'x' ? 'width' : 'height' as 'width' | 'height'
+const offsetProperty = camelize(`offset-${sizeProperty}`) as 'offsetHeight' | 'offsetWidth'
+
+function onBeforeEnter(el: HTMLExpandElement) {
+  el._parent = el.parentNode as (Node & ParentNode & HTMLElement) | null
+  el._initialStyle = {
+    transition: el.style.transition,
+    overflow: el.style.overflow,
+    [sizeProperty]: el.style[sizeProperty],
+  }
+}
+
+function onEnter(el: HTMLExpandElement) {
+  const initialStyle = el._initialStyle!
+
+  el.style.setProperty('transition', 'none', 'important')
+  // Hide overflow to account for collapsed margins in the calculated height
+  el.style.overflow = 'hidden'
+  const offset = `${el[offsetProperty]}px`
+
+  el.style[sizeProperty] = '0'
+
+  void el.offsetHeight // force reflow
+
+  el.style.transition = initialStyle.transition
+
+  el.classList.add(transitionClass)
+  requestAnimationFrame(() => {
+    el.style[sizeProperty] = offset
+  })
+}
+
+function onLeave(el: HTMLExpandElement) {
+  el._initialStyle = {
+    transition: '',
+    overflow: el.style.overflow,
+    [sizeProperty]: el.style[sizeProperty],
+  }
+
+  el.style.overflow = 'hidden'
+  el.style[sizeProperty] = `${el[offsetProperty]}px`
+  void el.offsetHeight // force reflow
+  el.classList.add(transitionClass)
+  requestAnimationFrame(() => (el.style[sizeProperty] = '0'))
+}
+
+function onAfterLeave(el: HTMLExpandElement) {
+  resetStyles(el)
+}
+
+function resetStyles(el: HTMLExpandElement) {
+  const size = el._initialStyle![sizeProperty]
+  el.style.overflow = el._initialStyle!.overflow
+  if (size != null) el.style[sizeProperty] = size
+  el.classList.remove(transitionClass)
+  delete el._initialStyle
+}
 </script>
 
 <template>
-
+  <transition :mode="mode"
+              @before-enter="onBeforeEnter"
+              @enter="onEnter"
+              @afterEnter="resetStyles"
+              @enter-cancelled="resetStyles"
+              @leave="onLeave"
+              @after-leave="onAfterLeave"
+              @leave-cancelled="onAfterLeave">
+    <slot></slot>
+  </transition>
 </template>
 
 <style>
+.expand-x-transition {
+  transition: width 282ms cubic-bezier(0.4, 0, 0.2, 1) !important;
+}
 
+.expand-y-transition {
+  transition: height 282ms cubic-bezier(0.4, 0, 0.2, 1) !important;
+}
 </style>
