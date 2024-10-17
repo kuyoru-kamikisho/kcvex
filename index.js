d4b86bf39ac17c04eeeb1f54c0ff0b8f9b46f18c
kuyoru <kuyoru@kamikisho.com>
2024-10-17 17:44:09 +0800

    添加索引文件

---
+++ index.js
@@ -0,0 +1,19 @@
+import K2ExpandTransition from './kc/K2ExpandTransition.vue'
+import K3ExpandTransition from './kc/K3ExpandTransition.vue'
+import K2Overlay from "./kc/K2Overlay.vue";
+import K3Overlay from "./kc/K3Overlay.vue";
+import KBtn from "./kc/KBtn.vue";
+import KDivider from "./kc/KDivider.vue";
+import KSheet from "./kc/KSheet.vue";
+import KStyle from "./kc/KStyle.vue";
+
+export {
+    K2ExpandTransition,
+    K3ExpandTransition,
+    K2Overlay,
+    K3Overlay,
+    KBtn,
+    KDivider,
+    KSheet,
+    KStyle
+}
