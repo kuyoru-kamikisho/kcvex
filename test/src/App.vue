5fe16b8f33ca8510007642e888c636e37b74e62f
kuyoru <kuyoru@kamikisho.com>
2024-10-24 16:20:59 +0800

    调整入口

---
+++ test/src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {K2Sheet} from 'kcvex'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <k2-sheet color="blue" width="100px" height="40px">测试</k2-sheet>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
