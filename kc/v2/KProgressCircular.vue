fb204e0eb8e114b93f278ede0c51b20fc19e301c
kuyoru <kuyoru@kamikisho.com>
2025-04-30 11:20:25 +0800

    + 新组件测试

---
+++ kc/v2/KProgressCircular.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "KProgressCircular",
+  props: {
+    color: {type: String, default: "red"},
+    indeterminate: {type: Boolean, default: false},
+    rotate: {type: [Number, String], default: 0},
+    size: {type: [Number, String], default: 32},
+    value: {type: [Number, String], default: 0},
+    width: {type: [Number, String], default: 4},
+  },
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
